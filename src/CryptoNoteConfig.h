137d06ba2b4fb14efe117ceda103989d7e02f1e8
netw3173 <113054391+netw3173@users.noreply.github.com>
2022-11-04 08:49:06 -0400

    Add files via upload

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -76,7 +76,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "pitcoin";
 //const char     GENESIS_COINBASE_TX_HEX[]                     = "";
-const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001ffffffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f6f68fa9d2df6b84a89301dfc82d27fcffd406825c0eeb99d7bc5a52796e5104";
+const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001ffffffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101d429e4a88948be72018450645edc39719125f8e52d8cb73d21ed56cf12df7520";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -87,9 +87,9 @@ const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by def
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
 //TODO This port will be used by the daemon to establish connections with p2p network
-const int      P2P_DEFAULT_PORT                              = 14796;
+const int      P2P_DEFAULT_PORT                              = 14794;
 //TODO This port will be used by the daemon to interact with simlewallet
-const int      RPC_DEFAULT_PORT                              = 25163;
+const int      RPC_DEFAULT_PORT                              = 25143;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -108,8 +108,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "194.31.55.145:14796",
-  "194.31.55.105:14796",
+  "194.31.55.145:14794",
+  "194.31.55.105:14794",
 };
 
 struct CheckpointData {
